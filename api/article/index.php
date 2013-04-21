71de0e65eb27163bce37623b4b5b32f483336a68
LaoLin <hi@laolin.com>
2013-04-22 01:21:29 +0800

    framework step2(form FT.com)
    see: http://labs.ft.com/2012/08/basic-offline-html5-web-app/

---
+++ api/article/index.php
@@ -0,0 +1,22 @@
+<?php
+// Convert RSS feed to JSON, stripping out all but basic HTML
+$rss = new SimpleXMLElement(file_get_contents('http://feeds2.feedburner.com/ft/tech-blog'));
+$xpath = '/rss/channel/item';
+$items = $rss->xpath($xpath);
+ 
+if ($items) {
+  $output = array();
+  foreach ($items as $id => $item) {
+ 
+    // This will be encoded as an object, not an array, by json_encode
+    $output[] = array(
+      'id' => $id + 1,
+      'headline' => strval($item->title),
+      'date' => strval($item->pubDate),
+      'body' => strval(strip_tags($item->description,'<p><br>')),
+      'author' => strval($item->children('http://purl.org/dc/elements/1.1/')->creator)
+    );
+  }
+}
+ 
+echo json_encode($output);
--- offline.manifest.php
+++ offline.manifest.php
@@ -2,7 +2,7 @@
 header("Content-Type: text/cache-manifest");
 ?>
 CACHE MANIFEST
-# 2013.4.21 V0.01.001 12
+# 2013.4.21 V0.01.001 123
 jquery.min.js
 index.html
 NETWORK:
