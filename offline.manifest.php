9c91a18bdd817ef49ec758270bdfe95116bffc80
LaoLin <hi@laolin.com>
2013-04-22 00:01:49 +0800

    framework (form FT.com) step 1
    see: http://labs.ft.com/2012/08/basic-offline-html5-web-app/

---
+++ offline.manifest.php
@@ -0,0 +1,9 @@
+<?php
+header("Content-Type: text/cache-manifest");
+?>
+CACHE MANIFEST
+# 2013.4.21 V0.01.001 1
+jquery.min.js
+index.html
+NETWORK:
+*
---
+++ source/application/applicationcontroller.js
@@ -0,0 +1,24 @@
+APP.applicationController = (function () {
+    'use strict';
+ 
+    function start(resources, storeResources) {
+ 
+        // Inject CSS into the DOM
+        $("head").append("<style>" + resources.css + "</style>");
+ 
+        // Create app elements
+        $("body").html('<div id="window"><div id="header"><h1>My Sites</h1></div><div id="body">Hello World 123!</div>');
+ 
+        // Remove our loading splash screen
+        $("#loading").remove();
+ 
+        if (storeResources) {
+          localStorage.resources = JSON.stringify(resources);
+        }
+ 
+    }
+ 
+    return {
+        start: start
+    };
+}());
